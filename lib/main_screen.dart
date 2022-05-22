c93c423f40a2f1c7dece2e3420dc281b00d5c2f6
anasshofyan <shofyananasmf@gmail.com>
2022-05-22 19:08:27 +0700

    feat: menambahkan fitur searching

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -3,199 +3,159 @@ import 'package:restaurants_app/search_menu.dart';
 import 'detail_screen.dart';
 import 'model/menu_item.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Pecel Lele Cak Su'),
-            backgroundColor: Colors.blue[900],
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SearchMenu()));
-                },
-              )
-            ],
-          ),
-          body: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              if (constraints.maxWidth <= 600) {
-                return const MenuList();
-              } else if (constraints.maxWidth <= 1200) {
-                return const MenuListGrid(gridCount: 4);
-              } else {
-                return const MenuListGrid(gridCount: 6);
-              }
-            },
-          ),
-        );
-      },
-    );
-  }
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class MenuListGrid extends StatelessWidget {
-  final int gridCount;
+class _MainScreenState extends State<MainScreen> {
+  bool isShow = false;
+  final controller = TextEditingController();
+  List<MenuItemResto> menuItemResto = allMenuItem;
 
-  const MenuListGrid({Key? key, required this.gridCount}) : super(key: key);
+  void showInput() {
+    setState(() {
+      isShow = !isShow;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scrollbar(
-      isAlwaysShown: true,
-      child: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: GridView.count(
-          crossAxisCount: gridCount,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-          children: menuItemList.map((item) {
-            return InkWell(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return DetailScreen(menuItem: item);
-                }));
-              },
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: Image.asset(
-                        item.imageAsset,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        item.name,
-                        style: const TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    // Padding(
-                    //   padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                    //   child: Text(
-                    //     item.location,
-                    //   ),
-                    // ),
-                  ],
-                ),
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
               ),
-            );
-          }).toList(),
+              onPressed: showInput,
+            )
+          ],
+          title: isShow
+              ? Text('Penyetan Cak Su')
+              : Container(
+                  width: double.infinity,
+                  height: 40,
+                  child: Center(
+                      child: TextField(
+                          onChanged: searchMenu,
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              hintText: 'Cari menu yang kamu suka..',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              contentPadding:
+                                  EdgeInsets.fromLTRB(10, 0, 10, 10)))),
+                ),
         ),
-      ),
-    );
-  }
-}
-
-class MenuList extends StatelessWidget {
-  const MenuList({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          final MenuItem menuItem = menuItemList[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen(menuItem: menuItem);
-              }));
-            },
-            child: Card(
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                        width: 90,
-                        height: 90,
-                        alignment: Alignment.center,
-                        child: Image.asset(menuItem.imageAsset)),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.builder(
+            itemCount: menuItemResto.length,
+            itemBuilder: (context, index) {
+              final menuItem = menuItemResto[index];
+              return InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DetailScreen(menuItem: menuItem);
+                  }));
+                },
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  Expanded(
-                    flex: 3,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(14, 0, 14, 0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            menuItem.name,
-                            style: const TextStyle(
-                                fontSize: 18.0, fontWeight: FontWeight.w800),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Row(children: [
-                            Container(
-                                height: 15,
-                                width: 15,
-                                alignment: Alignment.center,
-                                child:
-                                    Image.asset('assets/images/ic_star.png')),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(menuItem.rating,
-                                style: TextStyle(
-                                    fontSize: 14, fontWeight: FontWeight.w400))
-                          ]),
-                          SizedBox(
-                            height: 14,
-                          ),
-                          Text(
-                            menuItem.price,
-                            style: const TextStyle(
-                                fontSize: 16.0, fontWeight: FontWeight.w500),
-                          ),
-                        ],
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                            width: 90,
+                            height: 90,
+                            alignment: Alignment.center,
+                            child: Image.asset(menuItem.imageAsset)),
                       ),
-                    ),
-                  ),
-                  Expanded(
-                      flex: 1,
-                      child: OutlinedButton(
-                        onPressed: null,
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0))),
+                      Expanded(
+                        flex: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(14, 0, 14, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                menuItem.name,
+                                style: const TextStyle(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w800),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Row(children: [
+                                Container(
+                                    height: 15,
+                                    width: 15,
+                                    alignment: Alignment.center,
+                                    child: Image.asset(
+                                        'assets/images/ic_star.png')),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(menuItem.rating,
+                                    style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w400))
+                              ]),
+                              SizedBox(
+                                height: 14,
+                              ),
+                              Text(
+                                menuItem.price,
+                                style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
                         ),
-                        child: const Text('+',
-                            style: TextStyle(color: Colors.blue)),
-                      )),
-                ],
-              ),
-            ),
-          );
-        },
-        itemCount: menuItemList.length,
-      ),
-    );
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: OutlinedButton(
+                            onPressed: null,
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10.0))),
+                            ),
+                            child: const Text('+',
+                                style: TextStyle(color: Colors.blue)),
+                          )),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
+  }
+
+  void searchMenu(String query) {
+    final suggestions = allMenuItem.where((menuItem) {
+      final menuTitle = menuItem.name.toLowerCase();
+      final input = query.toLowerCase();
+
+      return menuTitle.contains(input);
+    }).toList();
+    setState(() => menuItemResto = suggestions);
+    print(query);
   }
 }
